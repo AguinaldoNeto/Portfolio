011af6b89bfbffe016cd6c81e1d742458533d4cd
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2024-02-05 22:18:58 -0300

    Criando o componente de projetos

---
+++ curso-angular-portfolio/src/app/modules/portfolio/components/projects/projects.component.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/projects.interface';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Batata</p>',
+      links: [
+        {
+          name: 'Conheça o blog',
+          href: 'https://vidafullstack.com.br'
+        }
+      ]
+    }
+  ]);
+}
